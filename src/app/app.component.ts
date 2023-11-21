fb5024c61a1f17129fee8dec26b145b024175a5c
AbdElRahman.Hisham <AbdElRahman.Hisham@CHQ-MIK>
2023-11-21 13:53:50 +0200

    wd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 import { SharedModule } from './shared/shared.module';
-// import { HttpClient } from '@angular/common/http';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
@@ -14,14 +11,6 @@ import { SharedModule } from './shared/shared.module';
     CommonModule,
     RouterOutlet,
     SharedModule,
-    // TranslateModule.forRoot({
-    //   loader: {
-    //     provide: TranslateLoader,
-    //     useFactory: httpTranslateLoader,
-    //     deps: [HttpClient],
-    //   },
-    //   isolate: false,
-    // }),
   ],
 
   templateUrl: './app.component.html',
@@ -30,7 +19,3 @@ import { SharedModule } from './shared/shared.module';
 export class AppComponent {
   title = 'structure-2';
 }
-
-// export function httpTranslateLoader(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
